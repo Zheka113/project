e0a756711ae05fbc75d3fbe77ef3da46c623ff9b
Evgeniy <zhekasenko@gmail.com>
2020-12-19 16:34:58 +0300

    navlink style

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -6,10 +6,10 @@ function NavBar(){
     return (
       <div className="NavBar">
         <h1>NavBar</h1>  
-        <div><NavLink to="/Music" activeClassName="selected">Music</NavLink></div>
-        <div><NavLink to="/Photo" activeClassName="selected">Photo</NavLink></div>
-        <div><NavLink to="/News" activeClassName="selected">News</NavLink></div>
-        <div><NavLink to="/Portfolio" activeClassName="selected">Portfolio</NavLink></div>
+        <div><NavLink to="/Music" activeStyle={{backgroundColor:'black', color: 'white'}}>Music</NavLink></div>
+        <div><NavLink to="/Photo" activeStyle={{backgroundColor:'black', color: 'white'}}>Photo</NavLink></div>
+        <div><NavLink to="/News" activeStyle={{backgroundColor:'black', color: 'white'}}>News</NavLink></div>
+        <div><NavLink to="/Portfolio" activeStyle={{backgroundColor:'black', color: 'white'}}>Portfolio</NavLink></div>
       </div>
     );
   }
